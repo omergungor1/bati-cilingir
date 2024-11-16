a9f8b30943602bd93bb57b2f9c2884ceeefeb95d
Ömer <omergungorco@gmail.com>
2024-11-16 15:09:03 +0300

    seo opt

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://www.bursabaticilingir.com', // Sitenizin URL'sini girin
+    generateRobotsTxt: true,  // robots.txt otomatik olarak oluşturulsun
+    changefreq: 'daily',      // Sayfa güncellenme sıklığı
+    priority: 0.7,            // Sayfanın önceliği
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -10,20 +10,22 @@ const roboto = Roboto({
 });
 
 export const metadata = {
-  title: "Bursa Batı Çilingir | 400TL Kilit Açma | Bursa",
+  title: "Bursa Çilingir ve Anahtarcı | Ekonomik Çilingir Hizmetleri| Bursa",
   description:
     "Bursa Batı Çilingir, ekonomik fiyatlarla kapı açma, anahtar değişimi, kasa açma ve oto çilingir hizmetleri sunuyoruz. 7/24 acil çilingir hizmeti. 0542 691 01 57 numaralı telefondan bize ulaşabilirsiniz. Bursa ekonomik çilingir.",
   image: "/favicon.ico",
   url: "https://www.bursabaticilingir.com",
   type: "website",
-  siteName: "Bursa Batı Çilingir | 400TL Kilit Açma | Bursa",
+  siteName: "Bursa Çilingir ve Anahtarcı | Ekonomik Çilingir Hizmetleri| Bursa",
   keywords: "Bursa çilingir, Bursa ekonomik çilingir, Bursa acil çilingir, Bursa anahtarcı, Bursa çilingir hizmetleri,Bursa kapı açma, Bursa oto çilingir, Bursa çelik kasa açma, Bursa uygun fiyat çilingir",
+  robots: "index, follow",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <Metadata {...metadata} />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-3PKMW7FKLT"></script>
         <script
           id="google-analytics"
